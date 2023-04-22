fb59f0cef0c9cd13fd6c7a717763cb7cf974637a
Riz Michael Sim <sim.rizmichael@seedtech.ph>
2023-04-22 23:11:06 +0800

    Initial Commit w/ ENVIRONMENT SETUP

---
+++ src/main/java/dev/lostdev/dopamine/clothing/DopamineClothingShopApplication.java
@@ -0,0 +1,13 @@
+package dev.lostdev.dopamine.clothing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopamineClothingShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DopamineClothingShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lostdev/dopamine/clothing/DopamineClothingShopApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lostdev.dopamine.clothing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DopamineClothingShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
